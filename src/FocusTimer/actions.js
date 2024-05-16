e736b7c9bd1fe180e110e3c5011f4bae7d775c4f
Rafael Ehlke <heeyrafaa@gmail.com>
2024-05-16 20:50:17 -0300

    add sync timer

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -3,10 +3,11 @@ import * as timer from "./timer.js";
 import * as el from "./elements.js"
 import * as sounds from "./sound.js"
 
+let startTime, elapsedTime = 0, running = true
 
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle("running");
-
+  startTime = performance.now() - elapsedTime;
   timer.countdown();
   sounds.buttonPressAudio.play()
 }
